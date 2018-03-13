4ffe57bce85c2336905ced3df36821fd4f2884b3
Unknown <pedrodlafuente@gmail.com>
2018-03-13 13:06:47 +0100

    Colectionfactory added

--- IteracionesColecciones/src/Principal.java
+++ IteracionesColecciones/src/Principal.java
@@ -2,7 +2,11 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
 
 	}
 
--- IteracionesColecciones/src/utils/ColectionFactory.java
+++ IteracionesColecciones/src/utils/ColectionFactory.java
@@ -1,5 +1,38 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
 public class ColectionFactory {
 
+	
+	
+	public static List<Integer> crearListadeEnteros(){
+		List<Integer> resultado = new ArrayList<>();
+		
+		for(int i = 1;i <=100;i++)resultado.add(i);
+		
+		
+		return resultado;
+	} 
+	
+	public static Map<Integer, Integer> crearMapDeEnteros(){
+		Map<Integer, Integer> resultado = new HashMap<>();
+		
+		for(int i = 1;i <=100;i++)resultado.put(i, i);
+		
+		return resultado;
+	}
+	
+	public static HashSet<Integer> crearHashSetDeEnteros(){
+		HashSet<Integer> resultado = new HashSet<>();
+		
+		for(int i =1; i<=100;i++)resultado.add(i);
+		
+		return resultado;
+	}
+	
 }
