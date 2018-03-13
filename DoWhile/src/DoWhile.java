442175f1372961bfda73c39998535aff236eb565
Unknown <pedrodlafuente@gmail.com>
2018-03-13 12:28:39 +0100

    dowhile

---
+++ DoWhile/src/DoWhile.java
@@ -0,0 +1,19 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		
+		int a = 100;
+		int cont = 0;
+		do {
+			cont++;
+			System.out.println(cont);
+			
+		} while (cont<a);
+		
+		
+		
+
+	}
+
+}
