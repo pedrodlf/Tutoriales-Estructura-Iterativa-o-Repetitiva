21f8b97c64b19f472548a8e23d13cb94e7527170
Unknown <pedrodlafuente@gmail.com>
2018-03-13 10:10:49 +0100

    final while

--- While/src/While.java
+++ While/src/While.java
@@ -1,4 +1,17 @@
 
 public class While {
 
+//	Programa Java que muestre los números del 1 al 100 utilizando la instrucción while
+	
+	public static void main(String[] args) {
+		
+		int a = 100; 
+		int count = 1;
+		while (count<=a) {
+			System.out.println(count);
+			count++;
+		}
+		
+	}
+	
 }
